551bf59ad18b9c843aff5f224154dbea2995b962
itsjuniordias <juniordias_@live.com>
2020-01-15 13:57:39 -0300

    Add cors in backend and SetState Web Components

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes')
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://junior:vestibulinho@cluster0-dlpmx.mongodb.net/RadarDev?retryWrites=true&w=majority', {
-  useNewUrlParser: true, useUnifiedTopology: true
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
