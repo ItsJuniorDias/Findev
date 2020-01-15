b31e0c376b02c76832bfc3df971eb9975e4efa0d
itsjuniordias <juniordias_@live.com>
2020-01-15 09:07:06 -0300

    Listing Devs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,35 +1,10 @@
 const {Router} = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
 
 const routes = Router();
 
-routes.post('/devs', async (request, response) => {
-  console.log(request.body);
-  const { github_username, techs, latitude, longitude }  = request.body;
-
-  const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-   //continuar
-   let { name = login, avatar_url, bio} = apiResponse.data;
-
-   const techsArray = techs.split(',').map(tech => tech.trim());
-
-   const location = {
-     type: 'Point',
-     coordinates: [longitude, latitude],
-   }
-
-  const dev = await Dev.create({
-     github_username,
-     name,
-     avatar_url,
-     bio,
-     techs: techsArray,
-     location,
-   }); 
-
-  return response.json(dev); 
-});
+routes.get('/devs', DevController.index);
+routes.post('/devs',DevController.store);
 
 module.exports = routes;
 
