35798c618f15879bf3215470a82baaefccb8bf0f
itsjuniordias <juniordias_@live.com>
2020-01-14 23:15:34 -0300

    Connected DataBase MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: "Hello OmniStack Mndo "});
+mongoose.connect('mongodb+srv://junior:vestibulinho@cluster0-dlpmx.mongodb.net/RadarDev?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true
+});
+
+app.use(express.json());
+// Query Params: req.query (filtros, ordenação, paginação)
+// Route Params: req.params (Identificar um recurso na alteração ou remoção)
+// Body: requesdt.body (Dados para criação ou alteração  de um registro)
+
+app.post('/users', (request, response) => {
+  console.log(request.body);
+  return response.json({ message: "Hello OmniStack Mundo "});
 });
 
 app.listen(3333);
