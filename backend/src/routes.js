1eeb827ce40023d4f5ff324c958d47cc4381ca14
itsjuniordias <juniordias_@live.com>
2020-01-15 08:54:51 -0300

    Add type Location User

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,7 +6,7 @@ const routes = Router();
 
 routes.post('/devs', async (request, response) => {
   console.log(request.body);
-  const { github_username, techs }  = request.body;
+  const { github_username, techs, latitude, longitude }  = request.body;
 
   const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
    //continuar
@@ -14,12 +14,18 @@ routes.post('/devs', async (request, response) => {
 
    const techsArray = techs.split(',').map(tech => tech.trim());
 
+   const location = {
+     type: 'Point',
+     coordinates: [longitude, latitude],
+   }
+
   const dev = await Dev.create({
      github_username,
      name,
      avatar_url,
      bio,
      techs: techsArray,
+     location,
    }); 
 
   return response.json(dev); 
