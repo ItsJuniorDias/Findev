2c9341e9a62398a76a764bd318da206abc95a1d4
itsjuniordias <juniordias_@live.com>
2020-01-21 10:50:58 -0300

    View Position in Map

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet } from 'react-native';
 import  MapView  from 'react-native-maps';
+import {requestPermissionsAsync, getCurrentPositionAsync,} from 'expo-location';
 
 // import { Container } from './styles';
 
 function Main()  {
-  return <MapView style={{ flex: 1}} />
+ const [currentRegion, setCurrentRegion ] = useState(null); 
+ 
 
+ useEffect(() => {
+   async function loadInitialPosition() {
+     const {granted} = await requestPermissionsAsync();
+
+     if(granted) {
+       const LocationData = getCurrentPositionAsync({
+         enableHighAccuracy: true,
+       });
+
+       const Position = (await LocationData).coords;
+
+       const {latitude, longitude } = Position;
+
+       setCurrentRegion({
+         latitude,
+         longitude,
+         latitudeDelta: 0.04,
+         longitudeDelta: 0.04,
+       })
+
+       console.log(Position);
+        
+       console.log(LocationData);
+     }
+   }
+   loadInitialPosition();
+ }, []);
+
+  if(!currentRegion) {
+    return null;
+  }
+
+  return <MapView initialRegion={currentRegion}  style={styles.map} />
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex:1
+  }
+})
 
 export default Main;
 
